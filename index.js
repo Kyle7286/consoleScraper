4db3d2fb068360228af6cbd495b20358088ebb8a
Brandon Haley <kyle7286@gmail.com>
2021-06-27 15:32:25 -0400

    added logic for target PS5

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const puppeteer = require('puppeteer');
 // ---------------------------------------------------
 
 // GameStop
-url_gameStop_playstation_dig =  "https://www.gamestop.com/video-games/playstation-5/consoles/products/playstation-5-digital-edition/11108141.html?condition=New"
+url_gameStop_playstation_dig = "https://www.gamestop.com/video-games/playstation-5/consoles/products/playstation-5-digital-edition/11108141.html?condition=New"
 url_gameStop_playstation_stan = "https://www.gamestop.com/video-games/playstation-5/consoles/products/playstation-5/11108140.html?condition=New"
 
 // BestBuy
 url_bestBuy_playstation_stan = "https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149"
-url_bestBuy_playstation_dig =   "https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161"
+url_bestBuy_playstation_dig = "https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161"
+
+// Target
+url_target_playstation_stan = "https://www.target.com/p/playstation-5-console/-/A-81114595"
+url_target_playstation_dig = "https://www.target.com/p/playstation-5-digital-edition-console/-/A-81114596"
 
 
 // ---------------------------------------------------
@@ -48,7 +52,7 @@ let report = {
 // ---------------------------------------------------
 
 // GAMESTOP - Digital & Standard Edition
-//  * This function will grab the availability of the PS5 from Gamestop websites
+//  * This function will grab the availability of the PS5
 const getGameStop_PS5 = async (url) => {
     try {
         // Create a browser session with headless true so the browser doesnt show (think silent mode)
@@ -86,8 +90,8 @@ const getGameStop_PS5 = async (url) => {
     }
 }
 
-// GAMESTOP - Digital & Standard Edition
-//  * This function will grab the availability of the PS5 from Gamestop websites
+// BESTBUY - Digital & Standard Edition
+//  * This function will grab the availability of the PS5
 const getBestBuy_PS5 = async (url) => {
     try {
         // Create a browser session with headless true so the browser doesnt show (think silent mode)
@@ -119,16 +123,59 @@ const getBestBuy_PS5 = async (url) => {
     }
 }
 
+// TARGET - Digital & Standard Edition
+//  * This function will grab the availability of the PS5
+const getTarget_PS5 = async (url) => {
+    try {
+        // Create a browser session with headless true so the browser doesnt show (think silent mode)
+        const browser = await puppeteer.launch({ headless: true });
+
+        // Create a new page
+        const page = await browser.newPage();
+
+        // Set userAgent so that websites don't block you
+        page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36')
+
+        // Visit the website
+        await page.goto(url)
+
+        // Wait for the element we are interested in to load
+        await page.waitForSelector('.h-text-orangeDark');
+
+        // Get the attribute data of the element
+        const avail = await page.evaluate('document.querySelector(".h-text-orangeDark").innerHTML')
+
+        // Close the browser session
+        await browser.close()
+
+        // Return the result
+        return avail
+
+    } catch (e) {
+        console.log('Error', e);
+    }
+}
+
 
 // ---------------------------------------------------
 //                  Init Function
 // ---------------------------------------------------
 
 const getAvailability = async () => {
+
+
+
+
+
     report.playstation.gamestop.dig = await getGameStop_PS5(url_gameStop_playstation_dig)
     report.playstation.gamestop.stan = await getGameStop_PS5(url_gameStop_playstation_stan)
+
     report.playstation.bestbuy.dig = await getBestBuy_PS5(url_bestBuy_playstation_dig)
     report.playstation.bestbuy.stan = await getBestBuy_PS5(url_bestBuy_playstation_stan)
+
+    report.playstation.target.stan = await getTarget_PS5(url_target_playstation_stan)
+    report.playstation.target.dig = await getTarget_PS5(url_target_playstation_dig)
+
 
     console.log(report);
 }
