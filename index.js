61a60d7c2ac40f94c9efa3005af61304a05e5ff5
Brandon Haley <kyle7286@gmail.com>
2021-07-09 09:45:54 -0400

    Refactored all logic to base it off the single purchase/available element for each store and converted the values to true/false depending on avail

--- index.js
+++ index.js
@@ -8,46 +8,26 @@ require('dotenv').config();
 // ---------------------------------------------------
 
 // GameStop
+// url_gameStop_playstation_dig = "https://www.gamestop.com/video-games/playstation-4/accessories/controllers/products/sony-dualshock-4-black-wireless-controller/10136481.html?condition=New"
+
 url_gameStop_playstation_dig = "https://www.gamestop.com/video-games/playstation-5/consoles/products/playstation-5-digital-edition/11108141.html?condition=New"
 url_gameStop_playstation_stan = "https://www.gamestop.com/video-games/playstation-5/consoles/products/playstation-5/11108140.html?condition=New"
 
 // BestBuy
 // url_bestBuy_playstation_stan = "https://www.bestbuy.com/site/sony-playstation-pulse-3d-wireless-headset-compatible-for-both-playstation-4-playstation-5-white/6430164.p?skuId=6430164"
+
 url_bestBuy_playstation_stan = "https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149"
 url_bestBuy_playstation_dig = "https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161"
 
 // Target
+// url_target_playstation_stan = "https://www.target.com/p/xbox-series-x-s-wireless-controller/-/A-81874852"
+
 url_target_playstation_stan = "https://www.target.com/p/playstation-5-console/-/A-81114595"
 url_target_playstation_dig = "https://www.target.com/p/playstation-5-digital-edition-console/-/A-81114596"
 
 // Amazon
 url_amazon_playstation_stan = "https://www.amazon.com/PlayStation-5-Console/dp/B08FC5L3RG/ref=sr_1_3?dchild=1&keywords=ps5&qid=1624822502&rnid=2941120011&s=videogames&sr=1-3"
 
-
-// ---------------------------------------------------
-//                Report Object
-// ---------------------------------------------------
-
-let report = {
-    playstation: {
-        gamestop: {
-            dig: "",
-            stan: "",
-        },
-        bestbuy: {
-            dig: "",
-            stan: "",
-        },
-        target: {
-            dig: "",
-            stan: "",
-        },
-        // amazon: {
-        //     dig: "",
-        //     stan: "",
-        // },
-    }
-};
 
 // ---------------------------------------------------
 //                  SCRAPERS
@@ -79,7 +59,7 @@ const getGameStop_PS5 = async (url) => {
             let availability = JSON.parse(element).productInfo.availability
 
             // parse data-attribute to an object and grab the value we want
-            return availability === "Not Available" ? "Not Available" : null;
+            return availability === "Not Available" ? false : true;
 
         })
 
@@ -116,7 +96,7 @@ const getBestBuy_PS5 = async (url) => {
         // Get the attribute data of the element
         const avail = await page.evaluate(() => {
             let element = document.querySelector(".add-to-cart-button").innerHTML;
-            return element === "Sold Out" ? element : null;
+            return element === "Sold Out" ? false : true;
         })
 
         // Close the browser session
@@ -151,8 +131,12 @@ const getTarget_PS5 = async (url) => {
 
         // Get the attribute data of the element
         const avail = await page.evaluate(() => {
-            let element = document.querySelector(".h-text-orangeDark");
-            return element ? element.innerText : null;
+            // let element = document.querySelector(".h-text-orangeDark");
+            let element = document.querySelector('[data-test="orderPickupButton"]');
+
+            return element ? true : false;
+
+
         })
 
         // Close the browser session
@@ -203,6 +187,19 @@ const getAmazon_PS5 = async (url) => {
 //                  Init Function
 // ---------------------------------------------------
 const getAvailability = async () => {
+    let consoleAvailable = false;
+
+    // Report Object
+    let report = {
+        gamestop_ps_dig: "",
+        gamestop_ps_stan: "",
+
+        bestbuy_ps_dig: "",
+        bestbuy_ps_stan: "",
+
+        target_ps_dig: "",
+        target_ps_stan: "",
+    };
 
     console.log("")
     console.log("-START-")
@@ -213,20 +210,22 @@ const getAvailability = async () => {
     let gx = await getGameStop_PS5(url_gameStop_playstation_stan)
     let gy = await getGameStop_PS5(url_gameStop_playstation_dig)
 
-    report.playstation.gamestop.stan = gx
-    report.playstation.gamestop.dig = gy
+    report.gamestop_ps_stan = false
+    report.gamestop_ps_dig = false
 
     console.log("======" + "GAMESTOP" + "======");
-    if (gx != "Not Available") {
+    if (gx) {
         console.log("Standard: " + chalk.green(url_gameStop_playstation_stan));
-        report.playstation.gamestop.stan = url_gameStop_playstation_stan;
+        report.gamestop_ps_stan = url_gameStop_playstation_stan;
+        consoleAvailable = true;
 
     } else {
         console.log("Standard: " + chalk.red(gx));
     }
-    if (gy != "Not Available") {
+    if (gy) {
         console.log("Digital: " + chalk.green(url_gameStop_playstation_dig));
-        report.playstation.gamestop.stan = url_gameStop_playstation_dig;
+        report.gamestop_ps_dig = url_gameStop_playstation_dig;
+        consoleAvailable = true;
     } else {
         console.log("Digital: " + chalk.red(gy));
     }
@@ -237,20 +236,22 @@ const getAvailability = async () => {
     let bx = await getBestBuy_PS5(url_bestBuy_playstation_stan)
     let by = await getBestBuy_PS5(url_bestBuy_playstation_dig)
 
-    report.playstation.bestbuy.stan = bx
-    report.playstation.bestbuy.dig = by
+    report.bestbuy_ps_stan = false
+    report.bestbuy_ps_dig = false
 
     console.log("======" + "BESTBUY" + "======");
-    if (bx != "Sold Out") {
+    if (bx) {
         console.log("Standard: " + chalk.green(url_bestBuy_playstation_stan));
-        report.playstation.bestbuy.stan = url_bestBuy_playstation_stan;
+        report.bestbuy_ps_stan = url_bestBuy_playstation_stan;
+        consoleAvailable = true;
 
     } else {
         console.log("Standard: " + chalk.red(bx));
     }
-    if (by != "Sold Out") {
+    if (by) {
         console.log("Digital: " + chalk.green(url_bestBuy_playstation_dig));
-        report.playstation.bestbuy.dig = url_bestBuy_playstation_dig;
+        report.bestbuy_ps_dig = url_bestBuy_playstation_dig;
+        consoleAvailable = true;
 
     } else {
         console.log("Digital: " + chalk.red(by));
@@ -262,33 +263,41 @@ const getAvailability = async () => {
     // Target
     let tx = await getTarget_PS5(url_target_playstation_stan)
     let ty = await getTarget_PS5(url_target_playstation_dig)
-    report.playstation.target.stan = tx
-    report.playstation.target.dig = ty
+    report.target_ps_stan = false
+    report.target_ps_dig = false
 
     console.log("======" + "TARGET" + "======");
-    if (tx != "Sold out") {
+
+    // Standard
+    if (tx) {
         console.log("Standard: " + chalk.green(url_target_playstation_stan));
-        report.playstation.target.stan = url_target_playstation_stan;
+        report.target_ps_stan = url_target_playstation_stan;
+        consoleAvailable = true;
     } else {
         console.log("Standard: " + chalk.red(tx));
     }
-    if (ty != "Sold out") {
-        console.log("Digital: " + chalk.green(url_target_playstation_dig));
-        report.playstation.target.dig = url_target_playstation_dig;
+
+    // Digital
+    if (ty) {
+        console.log("Digital: " + chalk.green(url_target_playstation_stan));
+        report.target_ps_stan = url_target_playstation_stan;
+        consoleAvailable = true;
     } else {
         console.log("Digital: " + chalk.red(ty));
     }
     console.log("==================");
 
     // Log Report object
-    // console.log(report);
+    console.log(report);
 
     // Amazon
     // report.playstation.amazon.stan = await getAmazon_PS5(url_amazon_playstation_stan)
     // console.log(report);
 
+    // Send email results if a console was found
+    if (consoleAvailable) { sendEmail(report); }
 
-    sendEmail();
+
 
 
     console.log("Sleeping...");
@@ -299,7 +308,29 @@ const getAvailability = async () => {
 
 }
 
-const sendEmail = () => {
+
+// ---------------------------------------------------
+//                  Email Function
+// ---------------------------------------------------
+const sendEmail = (report) => {
+
+
+    // Convert object into array and filter to only the results that matter
+    const array = Object.entries(report).filter(([key, value]) => { return value != false })
+    let string_list = ""
+    array.forEach(element => {
+        string_list += `<li>${element[0]}: ${element[1]}</li>\n`
+    });
+
+    console.log(string_list)
+
+
+    let body_html = `
+        <h1>Please review the below links!</h1>
+        <ul>
+        ${string_list}
+        </ul>
+    `
 
     // Auth
     const transporter = nodemailer.createTransport({
@@ -310,14 +341,12 @@ const sendEmail = () => {
         }
     })
 
-
     // Options
     const options = {
         from: process.env.USER,
         to: process.env.TO,
-        subject: "Test Email",
-        text: "This is a test email from my node application."
-
+        subject: "CONSOLE ALERT!!!! Check the links below immediately!",
+        html: body_html
     };
 
     transporter.sendMail(options, (err, info) => {
--- test.js
+++ test.js
@@ -1,48 +1,66 @@
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
 
-// TARGET - Digital & Standard Edition
-//  * This function will grab the availability of the PS5
-const getTarget_PS5 = async (url) => {
-    try {
-        // Create a browser session with headless true so the browser doesnt show (think silent mode)
-        const browser = await puppeteer.launch({ headless: true });
+// // TARGET - Digital & Standard Edition
+// //  * This function will grab the availability of the PS5
+// const getTarget_PS5 = async (url) => {
+//     try {
+//         // Create a browser session with headless true so the browser doesnt show (think silent mode)
+//         const browser = await puppeteer.launch({ headless: true });
 
-        // Create a new page
-        const page = await browser.newPage();
+//         // Create a new page
+//         const page = await browser.newPage();
 
-        // Set userAgent so that websites don't block you
-        page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36')
+//         // Set userAgent so that websites don't block you
+//         page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36')
 
-        // Visit the website
-        await page.goto(url)
+//         // Visit the website
+//         await page.goto(url)
 
-        // Wait for the element we are interested in to load
-        await page.waitForSelector('[data-test="flexible-fulfillment"]');
+//         // Wait for the element we are interested in to load
+//         await page.waitForSelector('[data-test="flexible-fulfillment"]');
 
-        // Get the attribute data of the element
-        const avail = await page.evaluate(() => {
+//         // Get the attribute data of the element
+//         const avail = await page.evaluate(() => {
 
-            let element = document.querySelector(".h-text-orangeDark");
-            return element ? element.innerText : null;
+//             let element = document.querySelector(".h-text-orangeDark");
+//             return element ? element.innerText : null;
 
-        })
+//         })
 
-        console.log(avail);
+//         console.log(avail);
 
-        // Close the browser session
-        await browser.close()
+//         // Close the browser session
+//         await browser.close()
 
-        // Return the result
-        return avail
+//         // Return the result
+//         return avail
 
-    } catch (e) {
-        console.log('Error', e);
-    }
+//     } catch (e) {
+//         console.log('Error', e);
+//     }
+// }
+
+// // Target
+// url_target_playstation_stan = "https://www.target.com/p/xbox-series-x-s-wireless-controller/-/A-81874852?preselect=82628663#lnk=sametab"
+// // url_target_playstation_stan = "https://www.target.com/p/playstation-5-digital-edition-console/-/A-81114596"
+
+// // Init
+// getTarget_PS5(url_target_playstation_stan);
+
+
+const obj = {
+    a: null,
+    b: null,
+    c: "www.google.com",
+    d: "www.goggles.com",
+    e: null
 }
 
-// Target
-url_target_playstation_stan = "https://www.target.com/p/xbox-series-x-s-wireless-controller/-/A-81874852?preselect=82628663#lnk=sametab"
-// url_target_playstation_stan = "https://www.target.com/p/playstation-5-digital-edition-console/-/A-81114596"
+// Convert object into array and filter to only the results that matter
+const array = Object.entries(obj).filter(([key, value]) => { return value != null })
+let string = ""
+array.forEach(element => {
+    string += `${element[0]}: ${element[1]} \n`
+});
 
-// Init
-getTarget_PS5(url_target_playstation_stan);
+console.log(string);
